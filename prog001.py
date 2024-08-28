082a2a46e03e27222025471948dc63ac9b045639
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-08-28 13:46:49 -0300

    exercícios básicos

---
+++ prog001.py
@@ -0,0 +1,12 @@
+"""
+prog001.py
+Mundo de 8x8
+O robô nasce em 4,1 e precisa caminhar para baixo até o fim do tabuleiro
+"""
+# ----------- NÃO APAGUE ---------------------------------------------
+from robozinho import *
+Mundo(8,8)
+Robot(4,1)
+# ----------- COMECE SEU PROGRAMA AQUI -------------------------------
+
+
